e058410041e0ff4f4855c106ca5bf2b83b1769f1
Fariz Hermawan <fariz.hermawan@traveloka.com>
2020-11-22 20:00:10 +0700

    Demo v1.0.0

---
+++ precache-manifest.7c76e9f6750fdf0cad1f900f2590b164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3717f387cf50b462d83b31704a90cdba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a25ee774b498461d5415",
+    "url": "./static/css/main.7b82eb61.chunk.css"
+  },
+  {
+    "revision": "a967039349eacea0ea62",
+    "url": "./static/js/2.b1558896.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "./static/js/2.b1558896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a25ee774b498461d5415",
+    "url": "./static/js/main.67792a20.chunk.js"
+  },
+  {
+    "revision": "841235164a6193e110e3",
+    "url": "./static/js/runtime-main.49f00976.js"
+  }
+]);
